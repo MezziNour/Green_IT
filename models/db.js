298a0d86af509a29f42641068cab3d1d3ded6e66
MezziNour <134057629+MezziNour@users.noreply.github.com>
2025-04-25 23:38:25 +0200

    sql

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "nounours",
+  password: " ", // Mettre son MDP mysql ici
   database: "todo_app",
 });
 
