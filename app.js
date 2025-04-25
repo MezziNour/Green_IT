c9e720c4fbf00273c6fc40954a1e3798920711bf
MezziNour <134057629+MezziNour@users.noreply.github.com>
2025-04-25 16:04:51 +0200

    opti images et vidéos + cache

--- app.js
+++ app.js
@@ -20,8 +20,42 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files and add cache control for videos and other static assets
+app.use(
+  "/videos", 
+  express.static(path.join(__dirname, "public/videos"), {
+    maxAge: "1y",  // Cache les vidéos pendant un an
+    setHeaders: (res, path) => {
+      if (path.endsWith(".webm") || path.endsWith(".mp4")) {
+        res.set("Cache-Control", "public, max-age=31536000, immutable");
+      }
+    }
+  })
+);
 
+app.use(
+  "/images", 
+  express.static(path.join(__dirname, "public/images"), {
+    maxAge: "30d", // Cache les images pendant 30 jours
+    setHeaders: (res, path) => {
+      if (path.endsWith(".webp") || path.endsWith(".jpg") || path.endsWith(".png")) {
+        res.set("Cache-Control", "public, max-age=2592000, immutable"); // 30 jours
+      }
+    }
+  })
+);
+
+// Serve other static files (like stylesheets)
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    maxAge: "7d", // Cache les autres fichiers statiques (styles, scripts) pendant 7 jours
+    setHeaders: (res, path) => {
+      res.set("Cache-Control", "public, max-age=604800, immutable"); // 7 jours
+    }
+  })
+);
+
+// Authentication and authorization middleware
 app.use((req, res, next) => {
   const openPaths = [
     "/",
